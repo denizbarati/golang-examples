8ab0a29e74faaac37ee0e42c1eb4e46688cc8385
denizbarati <d.barati@mybitmax.com>
2023-04-15 16:02:22 +0330

    prettier

--- range/main.go
+++ range/main.go
@@ -5,8 +5,21 @@ import "fmt"
 var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
 
 func main() {
+	Range()
+}
+
+func Range() {
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
 	// "i" is index of array and "v" is value of array
+
+	// also you can skip index or value by this syntax:
+	for index := range pow {
+		fmt.Println("index is: ", index)
+	}
+	// and to skip value :
+	for _, value := range pow {
+		fmt.Println("value is: ", value)
+	}
 }
