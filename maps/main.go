3e3f905b79b9ad65d8a753bfaf0e416fc128417d
denizbarati <fatemehbaratit@gmail.com>
2023-04-18 11:32:03 +0330

    add multi maps example

--- maps/main.go
+++ maps/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	maps()
+	maps2()
+	mutliMaps()
 }
 
 type Vertex struct {
@@ -16,5 +20,28 @@ func maps() {
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{12.02, 74.3}
 	m["aaa"] = Vertex{12.501, 0.0002}
-	fmt.Printf("%#v",m["aaa"])
+	fmt.Printf("%#v \n", m["aaa"])
+}
+
+func maps2() {
+	mapExample := map[string]Vertex{
+		"first ": {12, 34},
+		"second": {20, 5},
+	}
+	fmt.Println(mapExample)
+}
+
+func mutliMaps() {
+	var m = map[string]Vertex{
+		"Bell Labs": {40.68433, -74.39967},
+		"Google":    {37.42202, -122.08408},
+	}
+	m["Splice"] = Vertex{34.05641, -118.48175}
+	fmt.Printf("%v\n", m)
+	delete(m, "Splice")
+	fmt.Println(m["Splice"])
+	name, ok := m["Splice"]
+	fmt.Printf("key 'Splice' is present?: %t - value: %v\n", ok, name)
+	name, ok = m["Google"]
+	fmt.Printf("key 'Google' is present?: %t - value: %v\n", ok, name)
 }
